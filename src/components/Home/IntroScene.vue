434175586baade9cbb27f614991ac48032959428
Bociort Dinu <bociortdinu@gmail.com>
2023-10-03 15:32:50 -0400

    first commit

---
+++ src/components/Home/IntroScene.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="scene-intro" id="intro">
+    <div class="static-container">
+      <h1 class="title">
+        <span class="dinu">dinu</span>
+        <span class="func">.is()</span>
+      </h1>
+
+      <TextBlock>
+        <p class="-purple">Bociort Dinu</p>
+        <p class="-gray">Software engineer</p>
+      </TextBlock>
+    </div>
+  </section>
+</template>
+
+<script>
+import TextBlock from '../TextBlock.vue'
+
+export default {
+  name: 'IntroScene',
+  components: { TextBlock },
+}
+</script>
